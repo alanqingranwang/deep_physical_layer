a21054b1acb4b0047c4acb84a36abe967a701c66
Alan Wang <alanqingranwang@gmail.com>
2019-06-19 10:15:39 -0400

    kinda working fft

--- extra.py
+++ extra.py
@@ -7,67 +7,80 @@ from Dataset import Dataset
 from torch.optim import Adam, RMSprop
 import math
 from torch.autograd import Variable
+import scipy.signal as signal
 
 import imageio
 from pulseshape_lowpass import pulseshape_lowpass
+from tqdm import tqdm
+import torch.nn.functional as F
 
 # Machine learning parameters
 NUM_EPOCHS = 300
 BATCH_SIZE = 256
 
 # Comms parameters
-CHANNEL_USE = 4 # The parameter n
+CHANNEL_USE = 7 # The parameter n
 BLOCK_SIZE = 4 # The parameter k
+SAMPS_PER_SYMB = 6
 
 # Torch parameters
-USE_CUDA = True 
+USE_CUDA = True
 
 class Net(nn.Module):
-    def __init__(self, in_channels, compressed_dim, snr):
+    def __init__(self, in_channels, enc_compressed_dim, dec_compressed_dim, h_lowpass, snr):
         super(Net, self).__init__()
         self.in_channels = in_channels
-        self.compressed_dim = compressed_dim
+        self.enc_compressed_dim = enc_compressed_dim
+        self.dec_compressed_dim = dec_compressed_dim
         self.snr = snr
         self.encoder = nn.Sequential(
             nn.Linear(in_channels, in_channels),
             nn.LeakyReLU(inplace=True),
-            nn.Linear(in_channels, compressed_dim),
+            nn.Linear(in_channels, enc_compressed_dim),
         )
 
         self.decoder = nn.Sequential(
-            nn.Linear(compressed_dim, in_channels),
+            nn.Linear(dec_compressed_dim, in_channels),
             nn.LeakyReLU(inplace=True),
             nn.Linear(in_channels, in_channels)
-        ) 
+        )
+        self.h_lowpass = h_lowpass
+        self.conv1 = nn.Conv1d(1, 1, len(h_lowpass), padding=len(h_lowpass)-1, bias=False)
+        self.conv1.weight.data = h_lowpass.view(1, 1, -1)
+
         self.dropout = nn.Dropout(p=0.25)
         self.sig = nn.Sigmoid()
+
 
     def decode_signal(self, x):
         return self.decoder(x)
 
     def encode(self, x):
         x = self.encoder(x)
-        x = self.dropout(x)
-
-        # Normalization so that every element of x is normalized. Forces unit amplitude...? question about coding vs modulation
-        #x = (x / x.norm(dim=-1)[:, None])
+        #x = self.dropout(x)
+        # Normalization so that every row of x is normalized. Forces unit amplitude...
+        x = (x / x.norm(dim=-1)[:, None])
 
         # Normalization. Forces average power to be equal to n.
         #x = x / CHANNEL_USE
-        x = x / x.norm() * 70
+        #x = x / x.norm() * 70
         return x
 
     def forward(self, x):
         batch_size = len(x)
         x = self.encode(x)
 
-        x = pulseshape_lowpass(x, 20, batch_size, USE_CUDA)
+        #x = pulseshape_lowpass(x, SAMPS_PER_SYMB, batch_size, self.h_lowpass, USE_CUDA)
+        x = pad_within(x, stride=SAMPS_PER_SYMB)
+        x = torch.unsqueeze(x, 1)
+        x = self.conv1(x)
+        x = torch.squeeze(x)
 
         # Simulated Gaussian noise.
-        training_signal_noise_ratio = 10**(0.1*self.snr) 
+        training_signal_noise_ratio = 10**(0.1*self.snr)
 
         # bit / channel_use
-        communication_rate = BLOCK_SIZE / CHANNEL_USE   
+        communication_rate = BLOCK_SIZE / CHANNEL_USE
 
         noise = Variable(torch.randn(*x.size()) / ((2 * communication_rate * training_signal_noise_ratio) ** 0.5))
         if USE_CUDA: noise = noise.cuda()
@@ -77,6 +90,14 @@ class Net(nn.Module):
 
         x = self.sig(x)
         return x
+
+def pad_within(x, stride=2):
+    z = torch.zeros(len(x), x.shape[1]*stride)
+    index = torch.arange(0, z.shape[1], stride)
+    if USE_CUDA:
+        z = z.cuda()
+        index = index.cuda()
+    return z.index_copy_(1, index, x)
 
 def accuracy(preds, labels, to_print=False):
     if to_print:
@@ -89,7 +110,7 @@ def accuracy(preds, labels, to_print=False):
 
 
 if __name__ == "__main__":
-    train_new_model = True 
+    train_new_model = True
     if train_new_model == True:
         # Data generation
         train_data = torch.randint(2, (10000, BLOCK_SIZE)).float()
@@ -109,37 +130,42 @@ if __name__ == "__main__":
         snrs_db = np.linspace(-4, 9, num=14)
         snrs_db = [7]
         for snr in snrs_db:
-            loss_list = []
-            acc_list = []
+            # loss_list = []
+            # acc_list = []
 
-            # The input dimension to the autoencoder is the number of bits per codeword. 
+            # The input dimension to the autoencoder is the number of bits per codeword.
             # In this version, we want to feed in symbols and not a 1-hot vector.
             # The hidden dimension to the autoencoder should be n, the channel
             # use. This is because we wish the autoencoder to learn an optimal
             # coding scheme, so the middle layer should encode a vector of length
             # n.
-            model = Net(BLOCK_SIZE, compressed_dim=CHANNEL_USE, snr=snr)
-            if USE_CUDA: model = model.cuda()
-            optimizer = Adam(model.parameters(), lr=0.005)
+            #h_lowpass = np.loadtxt('h_lowpass.txt')
+            h_lowpass = torch.from_numpy(signal.firwin(10, 1/2)).float()
+            compressed_dim = len(h_lowpass) + CHANNEL_USE*SAMPS_PER_SYMB - 1
+            model = Net(in_channels=BLOCK_SIZE, enc_compressed_dim=CHANNEL_USE, dec_compressed_dim=compressed_dim, h_lowpass=h_lowpass, snr=snr)
 
-            #with imageio.get_writer('results/gifs/normalization.gif', mode='I') as writer:
-            for epoch in range(NUM_EPOCHS): 
-                for batch_idx, (batch, labels) in enumerate(training_loader):
+            model.conv1.weight.requires_grad = False
+
+            if USE_CUDA: model = model.cuda()
+            optimizer = Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.005)
+
+            for epoch in range(NUM_EPOCHS):
+                for batch_idx, (batch, labels) in tqdm(enumerate(training_loader), total=int(training_set.__len__()/BATCH_SIZE)):
                     if USE_CUDA:
                         batch = batch.cuda()
                         labels = labels.cuda()
                     output = model(batch)
                     loss = loss_fn(output, labels)
 
-                    model.zero_grad()
+                    optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
                     if batch_idx % (BATCH_SIZE-1) == 0:
                         pred = torch.round(output)
-                        acc = accuracy(pred, labels)  
+                        acc = accuracy(pred, labels)
                         print('Epoch %2d for SNR %s: loss=%.4f, acc=%.2f' % (epoch, snr, loss.item(), acc))
-                        loss_list.append(loss.item())
-                        acc_list.append(acc)
+                        # loss_list.append(loss.item())
+                        # acc_list.append(acc)
 
 #                        train_data = train_data.cuda()
 #                        train_codes = model.encode(train_data)
@@ -152,19 +178,36 @@ if __name__ == "__main__":
 #                        image = imageio.imread('results/images/bler_plot'+str(epoch)+'.png')
                         #writer.append_data(image)
 
+                train_data = train_data.cuda()
+                print('train_data', train_data[0])
+                train_code = model.encode(train_data[0].view(1, -1))
+                train_code = train_code.cpu().detach().numpy()
+                print('train_code', train_code[0])
+                fig = plt.figure()
+                L = 100
+                train_code_padded = np.zeros(L)
+                train_code_padded[:len(train_code[0])] = train_code[0]
+                H = np.fft.fft(train_code_padded)
+                plt.plot(np.abs(H))
+                plt.title('Epoch ' + str(epoch))
+                plt.savefig('results/images/fft_'+str(epoch)+'.png')
+                fig.clf()
+                plt.close()
+                image = imageio.imread('results/images/fft_'+str(epoch)+'.png')
+                writer.append_data(image)
 
+                # Validation
                 if USE_CUDA:
                     test_data = test_data.cuda()
                     test_labels = test_labels.cuda()
-
                 if epoch % 10 == 0:
                     val_output = model(test_data)
                     val_loss = loss_fn(val_output, test_labels)
                     val_pred = torch.round(val_output)
-                    val_acc = accuracy(val_pred, test_labels, True)  
+                    val_acc = accuracy(val_pred, test_labels, True)
                     print('Validation: Epoch %2d for SNR %s: loss=%.4f, acc=%.2f' % (epoch, snr, val_loss.item(), val_acc))
 
-            #torch.save(model.state_dict(), './models/model_state_validation')
+        #torch.save(model.state_dict(), './models/model_state_validation')
     else:
         test_labels = (torch.rand(1500) * CHANNEL_SIZE).long()
         test_data = torch.eye(CHANNEL_SIZE).index_select(dim=0, index=test_labels)
--- pulseshape_lowpass.py
+++ pulseshape_lowpass.py
@@ -5,6 +5,8 @@ from commpy.filters import rrcosfilter
 import torch
 import matplotlib.pyplot as plt
 
+import imageio
+
 def _downsample(sig, r):
     return sig[::r]
 
@@ -16,7 +18,7 @@ def _upsample(sig, r):
     return res
 
 def _convert_to_bit_stream(x, bits_per_code):
-    res = [] 
+    res = []
     for e in x:
         binary = bin(e)
         b_ind = binary.index('b')
@@ -29,75 +31,78 @@ def _convert_to_int_stream(x, M):
     res = []
     for i in range(0, len(x)-M+1, M):
         binary = x[i:i+M]
-        res.append(int("".join(str(x) for x in binary), 2)) 
+        res.append(int("".join(str(x) for x in binary), 2))
     return np.array(res)
 
-def pulseshape_lowpass(x, samps_per_symb, batch_size, USE_CUDA):
+def pulseshape_lowpass(x, samps_per_symb, batch_size, h_lowpass, USE_CUDA):
 
-        h_lowpass = np.loadtxt('h_lowpass.txt')
-        h_pulseshape = np.loadtxt('h_pulseshape.txt')
-        ###### Start of sketchy stuff #######
-        x = x.cpu().detach().numpy()
+    ###### Start of sketchy stuff #######
+    x = x.cpu().detach().numpy()
 
-        # We would like to learn an optimal coding for this given modulation scheme. 
-        # An optimal coding will be of whatever length x is, because the encoder will add redundancy
-        # to the original signal s. Therefore, the value of M should be the 2 to the power of the length of x, since the 
-        # number of total symbols to modulate is 2**len(x)
-        bits_per_code = x.shape[1]
+    # We would like to learn an optimal coding for this given modulation scheme.
+    # An optimal coding will be of whatever length x is, because the encoder will add redundancy
+    # to the original signal s. Therefore, the value of M should be 2 to the power of the length of x, since the
+    # number of total symbols to modulate is 2**len(x)
 
-        # Total number of possible codes (i.e. number of constellation points in a plot)
-        M = 2**bits_per_code
-        samps_per_symb = 20
+    res = []
+    # for b in range(batch_size):
+    #     x[b] = [int(np.round(x[b, i])) for i in range(len(x[b]))]
+    # x = x.astype(int)
 
-        # Simple PSK modulator
-        hmod = PSKModem(M)
+    for b in range(batch_size):
 
-        res = np.zeros((batch_size, x.shape[1]))
-        for b in range(batch_size):
-            x[b] = [int(np.round(x[b, i])) for i in range(len(x[b]))]
-        x = x.astype(int)
-        for b in range(batch_size):
-            sign_mask = [-1 if x[b][i] < 0 else 1 for i in range(len(x[b]))]
+        x_samples = _upsample(x[b], samps_per_symb)
 
-            # Convert to bit stream
-            x_bit = _convert_to_bit_stream(x[b], bits_per_code)
-            #print('x_bit', x_bit.shape)
+        # Filter with pulse shape filter first
+        #x_pulseshaped = np.convolve(x_samples, h_pulseshape, 'same') # Waveform with PSF
+        L = 100
+        # Then low pass filter
+        # if(b == 10):
+        #     zp = np.zeros(L)
+        #     zp[:len(x_samples)] = x_samples
+        #     fig = plt.figure()
+        #     X_samp = np.fft.fft(zp)
+        #     plt.plot(np.abs(X_samp))
+        #     plt.savefig('results/images/fft_test.png')
+        #     fig.clf()
+        #     plt.close()
 
-            # Modulate them
-            x_mod = hmod.modulate(x_bit)
-            x_samples = _upsample(x_mod, samps_per_symb);
-            #print('x_samples', x_samples.shape)
+        # if(b == 10):
+        #     zp = np.zeros(L)
+        #     zp[:len(h_lowpass)] = h_lowpass
+        #     fig = plt.figure()
+        #     H = np.fft.fft(zp)
+        #     plt.plot(np.abs(H))
+        #     plt.savefig('results/images/fft_lowpass.png')
+        #     fig.clf()
+        #     plt.close()
 
-            # Filter with pulse shape filter first
-            x_pulseshaped = np.convolve(x_samples, h_pulseshape, 'same') # Waveform with PSF
-            #print('x_pulseshaped', x_pulseshaped.shape)
+        x_lowpassed = np.convolve(x_samples, h_lowpass, 'full')
+        # if(b == 10):
+        #     zp = np.zeros(L)
+        #     zp[:len(x_lowpassed)] = x_lowpassed
+        #     fig = plt.figure()
+        #     X_low = np.fft.fft(zp)
+        #     plt.stem(np.abs(X_low))
+        #     plt.savefig('results/images/fft_test1.png')
+        #     fig.clf()
+        #     plt.close()
 
-            # Then low pass filter
-            x_pulseshaped_lowpassed = np.convolve(x_pulseshaped, h_lowpass, 'same')
-            #print('x_pulseshaped_lowpassed', x_pulseshaped_lowpassed.shape)
+        # Receive by match filtering first before downsample
+        #x_lowpassed = _downsample(x_lowpassed, samps_per_symb) * samps_per_symb
+        #if b == batch_size-1:
+        #    plt.figure()
+        #    plt.xlim([-1, 1])
+        #    plt.ylim([-1, 1])
+        #    #plt.scatter(y.real, y.imag, color = 'b', s = 1)
+        #    plt.scatter(x_mod.real, x_mod.imag, color = 'g')
+        #    plt.title('Pulse shaped receive symbols')
+        #    plt.savefig('./pulseshaped_receive_symbols')
+        #    plt.close()
 
-            # Receive by match filtering first before downsample
-            y = _downsample(np.convolve(x_pulseshaped_lowpassed, h_pulseshape, 'same'), samps_per_symb) * samps_per_symb
-            #print('y', y.shape)
-            if b == batch_size-1:
-                plt.figure()
-                plt.xlim([-1, 1])
-                plt.ylim([-1, 1])
-                #plt.scatter(y.real, y.imag, color = 'b', s = 1)
-                plt.scatter(x_mod.real, x_mod.imag, color = 'g') 
-                plt.title('Pulse shaped receive symbols')
-                plt.savefig('./pulseshaped_receive_symbols')
-                plt.close()
-            # Demodulate
-            demod_bits = hmod.demodulate(y, 'hard')
+        # Convert back to integers
+        res.append(x_lowpassed.real)
 
-            # Convert back to integers
-            res_b = _convert_to_int_stream(demod_bits, bits_per_code)
-            res_b = np.multiply(res_b, sign_mask)
-            if(np.sum(res_b - x[b]) != 0 ):
-                print('Modulation/demodulation error was ' + np.sum(res_b - x[b])) 
-            res[b] = res_b
-
-        res = torch.from_numpy(np.array(res)).float()
-        if USE_CUDA: res = res.cuda()
-        return res
+    res = torch.from_numpy(np.array(res)).float()
+    if USE_CUDA: res = res.cuda()
+    return res
