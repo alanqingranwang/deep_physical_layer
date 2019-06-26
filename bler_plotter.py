e9ac9f29cc45b1f1cc7a9e9288c42216300dacd4
Alan Wang <alanqingranwang@gmail.com>
2019-06-26 09:08:46 -0400

    created final ber plots comparing autoencoder model to theoretical and experimental qpsk

--- bler_plotter.py
+++ bler_plotter.py
@@ -2,38 +2,44 @@ import torch
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import numpy as np
-from extra import Net, accuracy, CHANNEL_USE, BLOCK_SIZE, SAMPS_PER_SYMB, LPF_CUTOFF, USE_CUDA
+from extra import Net, accuracy, CHANNEL_USE, BLOCK_SIZE, USE_CUDA, NUM_TAPS
+import scipy.signal as signal
 
-bpsk_from_matlab = [0.1861, 0.1584, 0.1306, 0.1038, 0.0786, 0.0563, 0.0375, 0.0229, 0.0125, 0.0060, 0.0024, 0.0008, 0.0002, 0.0000]
-snrs = np.linspace(-4, 9, num=14)
+# bpsk_from_matlab = [0.1861, 0.1584, 0.1306, 0.1038, 0.0786, 0.0563, 0.0375, 0.0229, 0.0125, 0.0060, 0.0024, 0.0008, 0.0002, 0.0000]
+mean_bers = np.loadtxt('mean_bers.txt')
+berawgn_bers = np.loadtxt('awgn_bers.txt')
+snrs = np.linspace(-5, 10, num=20)
 
-bler_autoenc = []
-train_data = torch.randint(2, (1500, BLOCK_SIZE)).float()
-train_labels = train_data
-if USE_CUDA:
-    test_data = test_data.cuda()
-    test_labels = test_labels.cuda()
+# bler_autoenc = []
+# test_data = torch.randint(2, (10000, BLOCK_SIZE)).float()
+# test_labels = test_data
+# if USE_CUDA:
+#     test_data = test_data.cuda()
+#     test_labels = test_labels.cuda()
 
-h_lowpass = torch.from_numpy(signal.firwin(100, LPF_CUTOFF)).float()
-for snr in snrs:
-    compressed_dim = len(h_lowpass) + CHANNEL_USE*SAMPS_PER_SYMB - 1
-    model = Net(in_channels=BLOCK_SIZE, enc_compressed_dim=CHANNEL_USE, dec_compressed_dim=compressed_dim, h_lowpass=h_lowpass, snr=7)
-    model.load_state_dict(torch.load('./models/model_state_lpf_'+str(snr)))
-    if USE_CUDA: model = model.cuda()
-    test_out = model(test_data)
-    pred = torch.round(test_out)
-    print(accuracy(pred, test_labels))
-    bler_autoenc.append(1-accuracy(pred, test_labels))
+# for snr in snrs:
+#     compressed_dim = NUM_TAPS + CHANNEL_USE - 1
+#     model = Net(in_channels=BLOCK_SIZE, enc_compressed_dim=CHANNEL_USE, dec_compressed_dim=compressed_dim, lpf_num_taps=NUM_TAPS, snr=7)
+#     model.load_state_dict(torch.load('./models/model_lpf_shift_' + str(snr)))
+#     model.eval()
+#     if USE_CUDA: model = model.cuda()
+#     test_out = model(test_data)
+#     pred = torch.round(test_out)
+#     print(accuracy(pred, test_labels))
+#     bler_autoenc.append(1-accuracy(pred, test_labels))
 
+bler_autoenc = [0.19135000000000002, 0.16894999999999993, 0.15187500000000004, 0.14487499999999998, 0.12974999999999984, 0.11457499999999998, 0.09182499999999999, 0.08404999999999996, 0.06242499999999997, 0.0489625000000000004, 0.03747499999999995, 0.02517499999999998, 0.015575000000000006, 0.014024999999999954, 0.014074999999999949, 0.015225000000000044, 0.014950000000000019, 0.014225000000000043, 0.015275000000000039, 0.017125000000000057]
 print(bler_autoenc)
-plt.semilogy(snrs, bpsk_from_matlab, ls = '-', color = 'b')
-plt.semilogy(snrs, bler_autoenc, ls = '--', color = 'r', marker = 'o')
+plt.semilogy(snrs, mean_bers, ls = '-', color = 'b')
+plt.semilogy(snrs, berawgn_bers, ls = '-', color = 'g')
+plt.semilogy(snrs, bler_autoenc, ls = '--', color = 'r')
 
 legend_strings = []
-legend_strings.append('Uncoded BPSK')
-legend_strings.append('Autoencoder (7,4)')
+legend_strings.append('Experimental QPSK with Pulseshaping')
+legend_strings.append('Theoretical QPSK')
+legend_strings.append('Autoencoder')
 plt.xlabel('SNR [dB]')
 plt.ylabel('Block Error Ratio')
 plt.legend(legend_strings, loc = 'lower left')
-plt.title('BLER vs SNR for Autoencoder and BPSK')
-plt.savefig('./sample.png')
+plt.title('BLER vs SNR for QPSK and Autoencoder Schemes')
+plt.savefig('./bpsk_vs_autoenc_shift.png')
--- extra.py
+++ extra.py
@@ -16,18 +16,18 @@ import torch.nn.functional as F
 import itertools
 
 # Machine learning parameters
-NUM_EPOCHS = 1400
+NUM_EPOCHS = 800
 BATCH_SIZE = 256
 LEARN_RATE = 0.01
-DROP_RATE = 0.25
+DROP_RATE = 0
 
 # Comms parameters
 CHANNEL_USE = 32 # The parameter n
 BLOCK_SIZE = 4 # The parameter k
 
 # Signal processing parameters
-# SAMPS_PER_SYMB = 6
 # LPF_CUTOFF = 1/2
+NUM_TAPS = 100
 
 # Torch parameters
 USE_CUDA = True
@@ -35,7 +35,7 @@ USE_CUDA = True
 
 
 class Net(nn.Module):
-    def __init__(self, in_channels, enc_compressed_dim, dec_compressed_dim, h_lowpass, snr):
+    def __init__(self, in_channels, enc_compressed_dim, dec_compressed_dim, lpf_num_taps, snr):
         super(Net, self).__init__()
         self.in_channels = in_channels
         self.enc_compressed_dim = enc_compressed_dim
@@ -43,7 +43,7 @@ class Net(nn.Module):
         self.snr = snr
         self.encoder = nn.Sequential(
             nn.Linear(in_channels, in_channels),
-            nn.LeakyReLU(inplace=True),
+            nn.PReLU(),
             nn.BatchNorm1d(in_channels),
             nn.Dropout(p=DROP_RATE),
             nn.Linear(in_channels, enc_compressed_dim),
@@ -51,16 +51,16 @@ class Net(nn.Module):
 
         self.decoder = nn.Sequential(
             nn.Linear(dec_compressed_dim, in_channels),
-            nn.LeakyReLU(inplace=True),
+            nn.PReLU(),
             nn.BatchNorm1d(in_channels),
+            # nn.Linear(in_channels, in_channels),
+            # nn.LeakyReLU(),
+            # nn.BatchNorm1d(in_channels),
             nn.Dropout(p=DROP_RATE),
             nn.Linear(in_channels, in_channels)
         )
-        self.h_lowpass = h_lowpass
-        self.conv1 = nn.Conv1d(1, 1, len(h_lowpass), padding=len(h_lowpass)-1, bias=False)
-
+        self.conv1 = nn.Conv1d(1, 1, lpf_num_taps, padding=lpf_num_taps-1, bias=False)
         self.sig = nn.Sigmoid()
-
 
     def decode(self, x):
         x = self.decoder(x)
@@ -68,14 +68,11 @@ class Net(nn.Module):
 
     def encode(self, x):
         x = self.encoder(x)
-        # Normalization so that every row of x is normalized. Forces unit amplitude...
-        x = self.in_channels**2 * (x / x.norm(dim=-1)[:, None])
+        # Normalization so that every row of x is normalized. Forces unit amplitude scaled to some factor
+        x = self.in_channels * (x / x.norm(dim=-1)[:, None])
 
         # Normalization. Scales points such that the maximum point is at the unit circle boundary.
-        # x = x / x.norm()
-        # max_x = torch.max(torch.norm(x, dim=1))
-        # scale_factor = 1/max_x
-        # x = x*scale_factor
+        # x = x / x.norm() * 2**BLOCK_SIZE * 100
         return x
 
     def lpf(self, x):
@@ -101,17 +98,14 @@ class Net(nn.Module):
         x = self.lpf(x)
         x = self.awgn(x)
         x = self.decode(x)
-        # Sigmoid for BCELoss
-        x = self.sig(x)
+        x = self.sig(x) # Sigmoid for BCELoss
         return x
 
 
-def accuracy(preds, labels, to_print=False):
-    if to_print:
-        print(torch.abs(preds-labels))
-    # complete accuracy by sample
+def accuracy(preds, labels):
+    # block-wise accuracy
     acc1 = torch.sum((torch.sum(torch.abs(preds-labels), 1)==0)).item()/(list(preds.size())[0])
-    # total accuracy over everything
+    # bit-wise accuracy
     acc2 = 1 - torch.sum(torch.abs(preds-labels)).item() / (list(preds.size())[0]*list(preds.size())[1])
     return acc2
 
@@ -120,7 +114,7 @@ if __name__ == "__main__":
     # Data generation
     train_data = torch.randint(2, (10000, BLOCK_SIZE)).float()
     train_labels = train_data
-    test_data = torch.randint(2, (1500, BLOCK_SIZE)).float()
+    test_data = torch.randint(2, (2500, BLOCK_SIZE)).float()
     test_labels = test_data
 
     # Data loading
@@ -139,24 +133,13 @@ if __name__ == "__main__":
     sample = torch.randint(2, (1, BLOCK_SIZE)).float().cuda()
     if USE_CUDA: sample = sample.cuda()
 
-    h_lowpass_25 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.25.txt'), (0, 3), 'constant')).float().cuda()
-    h_lowpass_30 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.30.txt'), (0, 4), 'constant')).float().cuda()
-    h_lowpass_35 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.35.txt'), (0, 4), 'constant')).float().cuda()
-    h_lowpass_40 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.40.txt'), (0, 0), 'constant')).float().cuda()
-    h_lowpass_45 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.45.txt'), (0, 5), 'constant')).float().cuda()
-    h_lowpass_50 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.50.txt'), (0, 1), 'constant')).float().cuda()
-    h_lowpass_55 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.55.txt'), (0, 5), 'constant')).float().cuda()
-    h_lowpass_60 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.60.txt'), (0, 5), 'constant')).float().cuda()
-    h_lowpass_65 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.65.txt'), (0, 5), 'constant')).float().cuda()
-    h_lowpass_70 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.70.txt'), (0, 5), 'constant')).float().cuda()
-    h_lowpass_75 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.75.txt'), (0, 5), 'constant')).float().cuda()
-    h_lowpass_80 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.80.txt'), (0, 5), 'constant')).float().cuda()
-    h_lowpass_85 = torch.tensor(np.pad(np.loadtxt('sharp_h_lowpass_0.85.txt'), (0, 3), 'constant')).float().cuda()
-
     # Training
-    snrs_db = np.linspace(-4, 9, num=14)
-    plt.figure()
+    snrs_db = np.linspace(-5, 10, num=20)
+    # plt.figure()
     for i, snr in enumerate(snrs_db):
+        if snr < 6.1:
+            continue
+
         val_loss_list_normal = []
         val_acc_list_normal = []
 
@@ -166,45 +149,22 @@ if __name__ == "__main__":
         # use. This is because we wish the autoencoder to learn an optimal
         # coding scheme, so the middle layer should encode a vector of length
         # n.
-        # h_lowpass = torch.from_numpy(signal.firwin(100, LPF_CUTOFF)).float()
-        h_lowpass_normal = torch.tensor(np.loadtxt('sharp_h_lowpass_0.40.txt')).float().cuda()
-
-        model = Net(in_channels=BLOCK_SIZE, enc_compressed_dim=CHANNEL_USE, dec_compressed_dim=len(h_lowpass_normal) + CHANNEL_USE - 1, h_lowpass=h_lowpass_normal, snr=snr)
+        # h_lowpass_normal = torch.tensor(np.loadtxt('sharp_h_lowpass_0.40.txt')).float().cuda()
 
         # The lowpass filter layer doesn't get its weights changed
-        model.conv1.weight.data = h_lowpass_normal.view(1, 1, -1)
-        model.conv1.weight.requires_grad = False
+
+        com_dim = NUM_TAPS+CHANNEL_USE-1
+        model = Net(in_channels=BLOCK_SIZE, enc_compressed_dim=CHANNEL_USE, dec_compressed_dim=com_dim, lpf_num_taps=NUM_TAPS, snr=snr)
 
         if USE_CUDA: model = model.cuda()
+
         optimizer = Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=LEARN_RATE)
 
         for epoch in range(NUM_EPOCHS):
-            if epoch < 100:
-                model.conv1.weight.data = h_lowpass_85.view(1, 1, -1)
-            elif epoch < 200:
-                model.conv1.weight.data = h_lowpass_80.view(1, 1, -1)
-            elif epoch < 300:
-                model.conv1.weight.data = h_lowpass_75.view(1, 1, -1)
-            elif epoch < 400:
-                model.conv1.weight.data = h_lowpass_70.view(1, 1, -1)
-            elif epoch < 500:
-                model.conv1.weight.data = h_lowpass_65.view(1, 1, -1)
-            elif epoch < 600:
-                model.conv1.weight.data = h_lowpass_60.view(1, 1, -1)
-            elif epoch < 700:
-                model.conv1.weight.data = h_lowpass_55.view(1, 1, -1)
-            elif epoch < 800:
-                model.conv1.weight.data = h_lowpass_50.view(1, 1, -1)
-            elif epoch < 900:
-                model.conv1.weight.data = h_lowpass_45.view(1, 1, -1)
-            elif epoch < 1000:
-                model.conv1.weight.data = h_lowpass_40.view(1, 1, -1)
-            elif epoch < 1100:
-                model.conv1.weight.data = h_lowpass_35.view(1, 1, -1)
-            elif epoch < 1200:
-                model.conv1.weight.data = h_lowpass_30.view(1, 1, -1)
-            else:
-                model.conv1.weight.data = h_lowpass_25.view(1, 1, -1)
+            cutoff = max(0.3, (NUM_EPOCHS-epoch-1)/NUM_EPOCHS)
+            h_lowpass = torch.from_numpy(signal.firwin(NUM_TAPS, cutoff)).float().cuda()
+            model.conv1.weight.requires_grad = False
+            model.conv1.weight.data = h_lowpass.view(1, 1, -1)
 
             model.train()
             for batch_idx, (batch, labels) in tqdm(enumerate(training_loader), total=int(training_set.__len__()/BATCH_SIZE)):
@@ -226,16 +186,16 @@ if __name__ == "__main__":
 
                     model.eval()
 
-                    # # Create gif of constellations
-                    # train_data = train_data.cuda()
-                    # train_codes = model.encode(train_data)
-                    # train_codes = train_codes.cpu().detach().numpy()
-                    # fig = plt.figure()
-                    # plt.scatter(train_codes[:, 0], train_codes[:, 1])
-                    # plt.title('Epoch ' + str(epoch))
-                    # plt.savefig('results/images/constellation/const_%3d.png' % (epoch))
-                    # fig.clf()
-                    # plt.close()
+                    # Create gif of constellations
+                    train_data = train_data.cuda()
+                    train_codes = model.encode(train_data)
+                    train_codes = train_codes.cpu().detach().numpy()
+                    fig = plt.figure()
+                    plt.scatter(train_codes[:, 0], train_codes[:, 1])
+                    plt.title('Epoch ' + str(epoch))
+                    plt.savefig('results/images/constellation/const_%3d.png' % (epoch))
+                    fig.clf()
+                    plt.close()
 
                     # Create gif of fft
                     sample = torch.tensor([1., 0., 1., 0.]).cuda()
@@ -253,7 +213,7 @@ if __name__ == "__main__":
                     lowpass_fft = torch.rfft(lowpass_pad, 1, normalized=False).cpu().detach().numpy()
                     plt.plot([np.sqrt(lowpass_fft[i, 0]**2 + lowpass_fft[i, 1]**2) for i in range(len(lowpass_fft))])
                     plt.title('Epoch ' + str(epoch))
-                    plt.savefig('results/images/fft_none/fft_%3d.png' % (epoch))
+                    plt.savefig('results/images/fft_none/fft_%s.png' % (str(epoch).zfill(4)))
                     fig.clf()
                     plt.close()
 
@@ -268,17 +228,15 @@ if __name__ == "__main__":
                         val_acc = accuracy(val_pred, test_labels)
                         val_loss_list_normal.append(val_loss)
                         val_acc_list_normal.append(val_acc)
-                        print('Validation: Epoch %2d for SNR %s: loss=%.4f, acc=%.2f' % (epoch, snr, val_loss.item(), val_acc))
+                        print('Validation: Epoch %2d for SNR %s and cutoff %s: loss=%.4f, acc=%.5f' % (epoch, snr, cutoff, val_loss.item(), val_acc))
                     model.train()
-    plt.figure()
-    plt.plot(val_loss_list_normal, label='normal')
-    plt.plot(val_loss_list_shift, label='shift')
-    plt.legend()
-    plt.savefig('./results/loss_shifting_exper.png')
+        torch.save(model.state_dict(), './models/model_lpf_shift_' + str(snr))
+    # plt.figure()
+    # plt.plot(val_loss_list_normal, label='normal')
+    # plt.legend()
+    # plt.savefig('./results/loss_shifting_exper.png')
 
-    plt.figure()
-    plt.plot(val_acc_list_normal, label='normal')
-    plt.plot(val_acc_list_shift, label='shift')
-    plt.legend()
-    plt.savefig('./results/acc_shifting_exper.png')
-    # torch.save(model.state_dict(), './models/model_state_lpf_' + str(snr))
+    # plt.figure()
+    # plt.plot(val_acc_list_normal, label='normal')
+    # plt.legend()
+    # plt.savefig('./results/acc_shifting_exper.png')
